738afd60675e90e2ebe7ce6587b06c426616ffd6
pttransfanygarudac-boop <pttransfanygarudac@gmail.com>
2026-09-06 17:29:49 +0700

    Add files via upload

---
+++ js/contact-data.js
@@ -0,0 +1,24 @@
+/* =========================================================
+   PT RENAI GVTL INDONESIA
+   OFFICIAL CONTACT DATA
+   ========================================================= */
+
+const RENAI_CONTACT = {
+
+    company: "PT RENAI GVTL Indonesia",
+
+    address: "Blok Langgeng, Desa Wanahayu, Kecamatan Maja, Kabupaten Majalengka, Jawa Barat, Indonesia",
+
+    map: "https://maps.app.goo.gl/w1bzrBz2b8p2Rz9S7?g_st=aw",
+
+    email: "renaigvtl@gmail.com",
+
+    whatsapp: "+62 813-8586-9321",
+
+    facebookProfile: "https://www.facebook.com/share/1EijJZuAN4/",
+
+    facebookGroup: "https://www.facebook.com/share/g/1DcFZBmMp3/",
+
+    tiktok: "https://vm.tiktok.com/ZS9kDddPDJUsf-qQQHe/"
+
+};
